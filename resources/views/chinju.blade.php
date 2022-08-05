f73a428614148b3bf8ff5b40e0c0201c9096116f
Duniya <duniyamohamedsaleem8@gmail.com>
2022-08-05 04:58:05 -0700

    project

---
+++ resources/views/chinju.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 align=center>Cake House</h1>
+    <h2>Customised theme cakes</h2>
+    <h3>doing birthday theme cakes,party and anniversary cakes,cup cakes,cake sicles and popsticks</h3>
+    <h3>whats app:0536418504</h3>
+    <h3>mobile:0547136985</h3>
+    <h3>malaz,Riyadh</h3>
+    <h3>Saudi Arabia</h3>
+</body>
+</html>
